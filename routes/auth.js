71a86d632cce73cb1738240d9e9fbcad76d6f35f
emixbal <emixbal@gmail.com>
2019-07-23 23:41:46 +0700

    register, auth/register router

--- routes/auth.js
+++ routes/auth.js
@@ -6,5 +6,6 @@ var AuthController = require('../controllers/AuthController');
 
 /* GET users listing. */
 router.post('/login', AuthController.login );
+router.post('/register', AuthController.register );
 
 module.exports = router;
