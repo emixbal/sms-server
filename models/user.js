28914f37e6447bdbec884c9da95a488c54fb3dde
emixbal <emixbal@gmail.com>
2019-07-23 22:57:10 +0700

    add defaultto UUID, defaultscope to hide password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 'use strict';
 var crypto = require('crypto');
+var uuid = require('uuid/v4');
+
 require('dotenv').config();
 
 module.exports = (sequelize, DataTypes) => {
@@ -8,12 +10,22 @@ module.exports = (sequelize, DataTypes) => {
     username: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING
-  }, {});
+  }, {
+    defaultScope: {
+      attributes: { exclude: ['password'] },
+    },
+    scopes: {
+      withPassword: {
+        attributes: { include: ['password'] },
+      }
+    }
+  });
   User.associate = function(models) {
     // associations can be defined here
   };
 
   User.beforeCreate((user, options) => {
+    user.id = uuid();
     var passwordHash = crypto.pbkdf2Sync(user.password, process.env.PASSWORD_KEY, 1000, 64, `sha512`).toString(`hex`);
     user.password = passwordHash;
     return user;
