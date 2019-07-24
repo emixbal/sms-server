f4ab43abe0637369c936b5238e6f3c7768e35d15
emixbal <emixbal@gmail.com>
2019-07-25 03:07:17 +0700

    register verify email

--- routes/auth.js
+++ routes/auth.js
@@ -7,5 +7,6 @@ var AuthController = require('../controllers/AuthController');
 /* GET users listing. */
 router.post('/login', AuthController.login );
 router.post('/register', AuthController.register );
+router.get('/verify', AuthController.verifyEmail );
 
 module.exports = router;
