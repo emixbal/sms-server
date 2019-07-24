a6c4e57e11fc476a281c5300c56b8ed8a824fcbb
emixbal <emixbal@gmail.com>
2019-07-25 03:37:35 +0700

    register new user route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+
+var model = require('../models');
+
 var UserController = require('../controllers/UserController');
 
-/* GET users listing. */
 router.get('/', UserController.index );
+router.post('/', UserController.store );
 
 module.exports = router;
