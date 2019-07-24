bbe2526665be2aa16b43e5203f573ce13e3c2fd5
emixbal <emixbal@gmail.com>
2019-07-25 02:50:56 +0700

    create send email on register

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 var validator = require('validator');
 var crypto = require('crypto');
 var jwt = require('jsonwebtoken');
+var nodemailer = require('nodemailer');
 
 
 var model = require('../models');
@@ -35,13 +36,40 @@ exports.login = function(req, res) {
     })
 };
 
+async function sendEmail(email, token){
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_ACCOUNT,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    });
+
+    const mailOptions = {
+        from: 'sender@email.com', // sender address
+        to: email, // list of receivers
+        subject: 'verify ur email', // Subject line
+        html: token// plain text body
+    };
+
+    await transporter.sendMail(mailOptions, function (err, info) {
+        if(err)
+            // console.log(err)
+            return res.send(err)
+        else
+            // console.log(info);
+            return res.send(err)
+    });
+}
+
 exports.register = async function(req, res) {
     if(validator.isEmail(req.body.email)==false)
         return res.status(400).send({'message':'Email kosong atau format salah!'})
+
     if(validator.isLength(req.body.password, {min:8, max: 10})==false)
         return res.status(400).send({'message':'Panjang password min 8 char, max 8 char'})
 
-    await model.User.findOne({ where: {email: req.body.email} })
+    await model.User.findOne({ where: {email: req.body.email},  })
     .then(function(user){
         if(user != null)
             return res.status(200).send({'message':'email anda telah terdaftar'})
@@ -51,18 +79,35 @@ exports.register = async function(req, res) {
         res.status(400).send({'message':'error'})
     })
 
-    	
-    await model.User.create(req.body)
-    .then(function(user){
+    var salt = crypto.randomBytes(16).toString('hex');
+    var emailToken = await crypto.pbkdf2Sync(req.body.email, salt, 1000, 32, `sha512`).toString(`hex`);
+	
+    await model.User.create({
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        emailToken: emailToken,
+    })
+    .then(async function(user){
+        await sendEmail(user.email, user.token)
+
         var newUser = {
-            id:user.id,
-            email:user.email,
-            username:user.username,
+            id: user.id,
+            email: user.email,
+            username: user.username,
         }
-        return res.status(201).send(newUser)
+        return res.status(201).send({
+            data: newUser,
+            message: 'verify your email!'
+        })
     })
     .catch(function(e){
         console.log('error register new user',e);
-        res.status(400).send({'message':'error'})
+        res.status(400).send({'message':e})
     })
+}
+
+exports.verifyEmail = function(req, res) {
+    
 }
