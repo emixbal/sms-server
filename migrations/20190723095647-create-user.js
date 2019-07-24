03a5d2d3c914d543ecc740de18db70c2f9354cf2
emixbal <emixbal@gmail.com>
2019-07-25 03:06:00 +0700

    set user migration, emailTOken as unique

--- migrations/20190723095647-create-user.js
+++ migrations/20190723095647-create-user.js
@@ -24,7 +24,8 @@ module.exports = {
         type: Sequelize.STRING
       },
       emailToken: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true,
       },
       smsToken: {
         type: Sequelize.STRING
