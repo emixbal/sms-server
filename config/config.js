9d85f6986d7a8a04ad330bfc947d925daa89abf6
iqbal <emixbal@gmail.com>
2019-08-29 15:34:34 +0700

    fix config

--- config/config.js
+++ config/config.js
@@ -20,7 +20,7 @@ module.exports = {
     "username": "jpccabvrrateqc",
     "password": "f59a044ef1ba9b8029d376f24beaaae7eadcac3f164f1d6b3f5264283541dac0",
     "database": "db08eas3ichar5",
-    "host": "ec2-79-125-2-142.eu-west-1.compute.amazonaws.com",
+    "host": "postgres://jpccabvrrateqc:f59a044ef1ba9b8029d376f24beaaae7eadcac3f164f1d6b3f5264283541dac0@ec2-79-125-2-142.eu-west-1.compute.amazonaws.com:5432/db08eas3ichar5",
     "dialect": "postgres",
     "operatorsAliases": false
   }
