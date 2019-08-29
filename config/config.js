7c97dbcc9a173f955d2b8d7214e6ae5b535b0321
iqbal <emixbal@gmail.com>
2019-08-29 16:38:28 +0700

    fix config

--- config/config.js
+++ config/config.js
@@ -17,8 +17,8 @@ module.exports = {
     "operatorsAliases": false
   },
   "production": {
-    "username": "jpccabvrrateqc",
-    "password": "f59a044ef1ba9b8029d376f24beaaae7eadcac3f164f1d6b3f5264283541dac0",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASS,
     "database": "db08eas3ichar5",
     "host": "ec2-79-125-2-142.eu-west-1.compute.amazonaws.com",
     "dialect": "postgres",
